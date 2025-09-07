7273eb8999e8f627c26cc17698ffb8626335d9d5
thatladytrucker <thatladytrucker@gmail.com>
2025-09-06 23:44:34 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+// Very small offline cache for baseline
+const CACHE = "hos-base-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+  // Icons will be cached automatically once present
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
