32b3239374693c0ffa7c400b94ce61920cdbc393
thatladytrucker <thatladytrucker@gmail.com>
2025-09-15 19:20:05 -0400

    Create service_worker.js
    new file

---
+++ service_worker.js
@@ -0,0 +1,13 @@
+/* service-worker.js — HOS Load Calculator v6.1a (baseline) */
+
+const CACHE_NAME = "hos-v6.1a-2025-09-15";
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icons/icon-180.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
