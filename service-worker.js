956aa086488b7e3c1ed93ae512b9ddd76cd937e0
thatladytrucker <thatladytrucker@gmail.com>
2025-09-15 21:04:19 -0400

    Update service-worker.js
    Updated icon name and location

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hos-load-calculator-v6-1a";
+const CACHE_NAME = "hos-load-calculator-v6-1a-3"; // bumped version
 const urlsToCache = [
   "/",
   "/index.html",
@@ -9,30 +9,19 @@ const urlsToCache = [
   "/icons/icon-192.png",
   "/icons/icon-512.png"
 ];
+
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
+
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });
+
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.keys().then((names) =>
+      Promise.all(names.map((n) => (n !== CACHE_NAME ? caches.delete(n) : undefined)))
+    )
   );
 });
